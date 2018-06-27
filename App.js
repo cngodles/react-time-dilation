c94fe5fde7480418f2926c9f7199e463ec0c4252
Clint <cngodles@gmail.com>
2018-06-27 16:59:24 -0400

    Calculations Now correct.

--- App.js
+++ App.js
@@ -7,9 +7,10 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      light: 100,
       years: '10',
       percent: '14.10673',
-      yearsatspeed: '9.9',
+      yearsatspeed: '9.9000',
       count:1
     };
   }
@@ -17,22 +18,26 @@ export default class App extends React.Component {
   //shouldComponentUpdate()
 
   render() {
+    let c2 = Math.pow(this.state.light, 2);
+    let v2 = Math.pow(parseFloat(this.state.percent), 2);
+    let squareme = 1-(v2/c2);
+    let dilated = (parseFloat(this.state.years) * Math.sqrt(squareme));
     return (
       <View style={styles.container}>
         <Text style={styles.h1}>Time Dilation</Text>
         <TextInput
-            style={{height: 60, borderColor: 'gray', borderWidth: 3, borderRadius:5, width: 140, padding:10, fontSize:30, fontWeight:'bold', textAlign:'center'}}
+            style={{height: 60, borderColor: 'gray', borderWidth: 3, borderRadius:5, width: 140, padding:10, margin:5, fontSize:30, fontWeight:'bold', textAlign:'center'}}
             onChangeText={(years) => this.setState({years})}
             keyboardType="numeric"
             value={this.state.years}
           />
-          <TextInput
-            style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 240, padding:10, fontSize:24}}
+        <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 200, padding:10, margin:5, fontSize:24}}
             onChangeText={(percent) => this.setState({percent})}
             value={this.state.percent}
           />
-        <Text style={styles.h2}>{(parseFloat(this.state.years) * Math.sqrt(1 - Math.pow(parseFloat(this.state.percent), 2)/Math.pow(light, 2))).toFixed(5)} Years.</Text>
-        <Text style={{padding:10}}>Add number of years in first field, add in a percentage speed of light in the second, and get a result that is the number of years that would pass for the person going that speed.</Text>
+        <Text style={styles.h2}>In {dilated.toFixed(2)} year(s) for you, stationary person will have seen {this.state.years} year(s) pass.</Text>
+        <Text style={{padding:20,fontSize:16}}>Directions: Add number of years in first field, add in a percentage speed of light in the second field.</Text>
       </View>
     );
   }
@@ -48,9 +53,11 @@ const styles = StyleSheet.create({
   h1: {
     fontSize:42,
     fontWeight:'bold',
+    margin:20,
   },
   h2: {
-    fontSize:28,
+    fontSize:18,
     fontWeight:'bold',
+    padding:20,
   }
 });
